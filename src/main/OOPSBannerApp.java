600a47b4268c752b3cee0eab5e70f5ec9c462684
jaysi1701 <jaysigaur9449@gmail.com>
2026-04-06 11:26:48 +0530

    UC2 modular working

---
+++ src/main/OOPSBannerApp.java
@@ -0,0 +1,9 @@
+package main;
+
+import utils.BannerPrinter;
+
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        BannerPrinter.printBanner();
+    }
+}
---
+++ src/utils/BannerPrinter.java
@@ -0,0 +1,12 @@
+package utils;
+
+public class BannerPrinter {
+
+    public static void printBanner() {
+        System.out.println( "***   ***   *****  ***** ");
+        System.out.println("*   * *   *  *   *  *     ");
+        System.out.println("*   * *   *  *****  ***   ");
+        System.out.println("*   * *   *  *      *     ");
+        System.out.println(" ***   ***   *      ***** ");
+    }
+}
