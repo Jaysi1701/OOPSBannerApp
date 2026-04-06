4ffce9f44788e2f6a9d3016798aed48da205c2d1
jaysi1701 <jaysigaur9449@gmail.com>
2026-04-06 10:40:28 +0530

    UC1 completed

---
+++ OOPSBannerApp.java
@@ -0,0 +1,10 @@
+/**
+ * OOPS Banner App UC1
+ * Prints OOPS to console
+ */
+
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
